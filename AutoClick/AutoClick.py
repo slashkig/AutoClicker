d113b4e49ef293356b1cbe106e8a96f42f0b59ec
slashkig <161779209+slashkig@users.noreply.github.com>
2024-09-09 22:10:40 -0400

    Updated

--- AutoClick/AutoClick.py
+++ AutoClick/AutoClick.py
@@ -1,14 +1,26 @@
 import pyautogui, time
+from pynput import keyboard
+
+auto = True
 
 def main():
+    global auto
     time.sleep(10)
-    while True:
-        try:
-            x, y = pyautogui.position()
-            pyautogui.click(x, y)
-            time.sleep(0.1)
-        except KeyboardInterrupt:
-            return
+    while auto:
+        x, y = pyautogui.position()
+        pyautogui.click(x, y)
+        time.sleep(0.1)
+
+def on_press(key):
+    global auto
+    if key == keyboard.Key.esc:
+        auto = False
+        return False
 
 if __name__ == "__main__":
+    print("Running autoclicker")
+    listener = keyboard.Listener(on_press = on_press)
+    listener.start()
     main()
+    listener.join()
+    print("Stopping autoclicker\n")
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # AutoClick
+There will be a 10 second wait time before autoclicking starts. Will autoclick every 0.1 seconds until the `Escape` key is pressed.
+
+### To install:
+Download the `.py` file.
+
+Open the terminal in the folder the autoclick file is located in.
+
+You will need the `pyautogui` and `pynput` library. If you don't have them, run the following commands in the terminal: `pip install pyautogui` and `pip install pynput`.
+
+Run `python AutoClick.py`.
+
+Use the `Escape` key to stop the autoclicker.
